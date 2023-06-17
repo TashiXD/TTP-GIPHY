0b26896a06f49f3c734cee53e996ce7274ff54ef
Tashi <tashishyangba23@gmail.com>
2023-06-17 19:22:10 -0400

    edited api key

--- src/components/TrendingGif/TrendingGif.jsx
+++ src/components/TrendingGif/TrendingGif.jsx
@@ -12,11 +12,7 @@ const TrendingGif = () => {
   useEffect(() => {
     async function fetchTrendingGif() {
       try {
-        const list = await axios.get("https://api.giphy.com/v1/gifs/trending", {
-          params: {
-            api_key: "qFb2hJrxjWy2Ezfe5hkxMyK3nk7nL2DR",
-          },
-        });
+        const list = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=qFb2hJrxjWy2Ezfe5hkxMyK3nk7nL2DR`);
         setTrendingGif(list.data.data);
       } catch (error) {
         console.log(error);
