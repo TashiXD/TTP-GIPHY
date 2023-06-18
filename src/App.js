046175cd94c11a1661539b03057e00d79ab4cbd5
Tashi <tashishyangba23@gmail.com>
2023-06-18 09:59:27 -0400

    fetched searched gif

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { TrendingGif } from './components';
+import { TrendingGif,SearchGif } from './components';
 function App() {
   return (
     <div className="App">
       <h1>Gif search</h1>
       <TrendingGif />
+      <SearchGif />
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 //Barrel file for container components, which will be used to compose our main App component
 export {default as TrendingGif} from "./TrendingGif/TrendingGif";
+export {default as SearchGif} from "./SearchGif/SearchGif";
