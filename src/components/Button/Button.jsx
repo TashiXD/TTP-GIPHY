2fdb2bc3916dfa42c69b0f0dcc360b89d977e004
Tashi <tashishyangba23@gmail.com>
2023-06-18 20:45:05 -0400

    search bar functional, removed button component usage

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./buttonStyle.css";
 
 //this component returns a button with the name that is passed down
 const Button = (props) => {
+  const handleSearchGif = (searchTerm) => {
+    console.log(searchTerm);
+  };
   return (
     <div>
-      <button className='button'>{props.btnName}</button>
+      {props.btnName !== "randomBtn" ? (
+        <button className="button">{props.btnName}</button>
+      ) : <button className="button" onClick={handleSearchGif(props.searchTerm)}>
+      {props.btnName}
+    </button>}
     </div>
-  )
-}
+  );
+};
 
 export default Button;
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
+import SearchGif from "../SearchGif/SearchGif";
 import "./topbar.css";
 /**
  * This component creates a top bar with:
@@ -8,13 +9,22 @@ import "./topbar.css";
  * - two buttons : search and a random button
  */
 const Topbar = () => {
+const[searchTerm, setSearchTerm] = useState("");
+
+const handleSearchChange = (event)=>{
+  setSearchTerm(event.target.value);
+}
+  const handleSearchGif =()=>{
+    <SearchGif searchTerm={searchTerm}/>
+  }
+
   return (
     <div className="topBar">
       <h1>Gif Finder</h1>
-      <div className="btn">
-        <input placeholder="search for gif"></input>
-        <Button btnName="search" />
-        <Button btnName="Random" />
+      <div className="search">
+        <input type="text" id="searchTerm" placeholder="search for gif" name="search" onChange={handleSearchChange}></input>
+        <button className="button" onClick={handleSearchGif}>Search</button>
+        <button className="button">Random</button>
       </div>
     </div>
   );
